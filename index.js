01bbc2d38b97637be6e6ab630a9fdd612c0c3b7d
juentingshie <t104340042@ntut.org.tw>
2021-05-26 16:21:26 +0100

    successfully create NTUST netflow watcher w/ cors

--- index.js
+++ index.js
@@ -3,110 +3,112 @@ var root = '/'
 if (window.location.host == 'juentingshie.github.io') {
     root = 'https://juentingshie.github.io/Vue.js/'
 }
+
+// console.log(window.location.host)
 // defines
 
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
 });
 
 var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: '網頁載入於 ' + new Date().toLocaleString()
-  }
+    el: '#app-2',
+    data: {
+        message: '網頁載入於 ' + new Date().toLocaleString()
+    }
 });
 
 var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: false
-  }
+    el: '#app-3',
+    data: {
+        seen: false
+    }
 });
 
 var app3_1 = new Vue({
-  el: '#app-3_1',
-  data: {
-    seen: false,
-    message: 'hehehehehehe'
-  },
-  methods: {
-    toggleEle: function () {
-      this.seen = !this.seen
+    el: '#app-3_1',
+    data: {
+        seen: false,
+        message: 'hehehehehehe'
+    },
+    methods: {
+        toggleEle: function () {
+            this.seen = !this.seen
+        }
     }
-  }
 
 })
 
 var app3_2 = new Vue({
-  el: '#app-3_2',
-  data: {
-    message: 'hehehehehehehehe'
-  }
+    el: '#app-3_2',
+    data: {
+        message: 'hehehehehehehehe'
+    }
 })
 
 var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [{
-        text: '學習 JavaScript'
-      },
-      {
-        text: '學習 Vue'
-      },
-      {
-        text: '整個項目'
-      }
-    ]
-  }
+    el: '#app-4',
+    data: {
+        todos: [{
+            text: '學習 JavaScript'
+        },
+        {
+            text: '學習 Vue'
+        },
+        {
+            text: '整個項目'
+        }
+        ]
+    }
 });
 
 var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: '我的雲端硬碟'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+    el: '#app-5',
+    data: {
+        message: '我的雲端硬碟'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
-  }
 });
 
 var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: 'Hello Vue!'
-  }
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
 });
 
 Vue.component('todo-item', {
-  // todo-item 组件现在接受一个
-  // "prop"，类似于一个自定义 attribute。
-  // 这个 prop 名为 todo。
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
+    // todo-item 组件现在接受一个
+    // "prop"，类似于一个自定义 attribute。
+    // 这个 prop 名为 todo。
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
 })
 var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    List: [{
-        id: 0,
-        text: 'hehe0'
-      },
-      {
-        id: 1,
-        text: 'hehe1'
-      },
-      {
-        id: 2,
-        text: 'hehe2'
-      },
-    ]
-  }
+    el: '#app-7',
+    data: {
+        List: [{
+            id: 0,
+            text: 'hehe0'
+        },
+        {
+            id: 1,
+            text: 'hehe1'
+        },
+        {
+            id: 2,
+            text: 'hehe2'
+        },
+        ]
+    }
 })
 
 
@@ -114,7 +116,7 @@ Vue.component('header-items', {
     props: ['header'],
     // template: '<a href={{ header.url }}>{{ header.text }}</a>'
     template: '<li><a :href="`${header.url}`">{{header.text}}</a></li>'
-  })
+})
 var app_header = new Vue({
     el: '#header',
     data: {
@@ -125,8 +127,6 @@ var app_header = new Vue({
     }
 })
 
-console.log(window.location.host)
-
 function toggle_seen(element) {
-  app3.seen = !app3.seen
+    app3.seen = !app3.seen
 }
--- netflow.js
+++ netflow.js
@@ -1,13 +1,18 @@
+// defines
 var root = '/'
 if (window.location.host == 'juentingshie.github.io') {
     root = 'https://juentingshie.github.io/Vue.js/'
 }
 
+// console.log(window.location.host)
+// defines
+
+// headers
 Vue.component('header-items', {
     props: ['header'],
     // template: '<a href={{ header.url }}>{{ header.text }}</a>'
     template: '<li><a :href="`${header.url}`">{{header.text}}</a></li>'
-  })
+})
 var app_header = new Vue({
     el: '#header',
     data: {
@@ -17,3 +22,87 @@ var app_header = new Vue({
         ]
     }
 })
+// headers
+
+Vue.component('ips', {
+    props: ['ip'],
+    template: '<li><p>{{ ip.ip }}</p> \
+                <p><button v-on:click="getflow(this)" :value= "`${ip.ip}`"> Get flow</button> {{ ip.flow }}</p > ',
+    methods: {
+        getflow: async function () {
+            var ip = this.$el.ownerDocument.activeElement.value
+            var index
+            ipof417.List.forEach(element => {
+                if (element.ip == ip) {
+                    index = ipof417.List.indexOf(element)
+                }
+            });
+            ipof417.List[index].flow = await caller(ip)
+        }
+    }
+})
+var ipof417 = new Vue({
+    el: '#ips',
+    data: {
+        List: [
+            // { ip: '140.118.5.xxx', flow: "NaN" },
+        ]
+    },
+    methods: {
+        getflow: async function () {
+            for (var i = 0; i < this.List.length; i++) {
+                // console.log(this.List[i])
+                // console.log(this.List[i].ip)
+                // console.log(await caller(this.List[i].ip))
+                this.List[i].flow = await caller(this.List[i].ip)
+            }
+            // console.log(this.flow)
+        }
+    },
+    created: function () {
+        var i = 2
+        while (i < 254) {
+            this.List.push({ ip: '140.118.5.' + i.toString(), flow: 'NaN' })
+            i++
+        }
+    }
+    // created: function () {
+    //     this.getflow()
+    // }
+})
+
+async function create_fetch(ip) {
+    // var cors = "https://cors-anywhere.herokuapp.com/"
+    var cors = "https://api.allorigins.win/get?url="
+
+    var now = new Date(Date.now());
+
+    var today = now.toLocaleDateString("zh-TW")
+
+    var dt = today + "T00:00:00.000"
+
+    // console.log(dt)
+
+
+    return fetch(`https://api.allorigins.win/get?url=${encodeURIComponent("https://network.ntust.edu.tw/flowStatistics/getFlowData?ip=" + ip + "&dt=" + dt + "&units=2")}`)
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Network response was not ok.')
+        })
+        .then((c) => {
+            // console.log(c.contents)
+            return JSON.parse(c.contents)
+            // jsonData['items'][0]['totflow']
+        })
+        .then((jsonData) => {
+            // console.log(jsonData['items'][0]['totflow'])
+            return jsonData['items'][0]['totflow']
+        })
+}
+
+async function caller(ip) {
+    var flow = await create_fetch(ip)
+
+    // console.log(flow)
+    return flow
+}
